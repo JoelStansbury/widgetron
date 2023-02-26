b4ced766a6f3221576545b27bd2bde081d89b5a4
JoelStansbury <stansbury.joel@gmail.com>
2023-02-26 16:02:25 -0500

    switch to electron-builder

--- src/widgetron/__main__.py
+++ src/widgetron/__main__.py
@@ -6,6 +6,22 @@ import shutil
 import sys
 from pathlib import Path
 from subprocess import call
+import os
+import zipfile
+    
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(
+                os.path.join(root, file), 
+                os.path.relpath(
+                    os.path.join(root, file), 
+                    os.path.join(path, '..')
+                )
+            )
+
+
 
 from .jinja_functions import render_templates
 
@@ -17,9 +33,6 @@ WIN = platform.system() == "Windows"
 LINUX = platform.system() == "Linux"
 OSX = platform.system() == "Darwin"
 
-INSTALL_ELECTRON = "npm install --save-dev electron"
-INSTALL_ELECTRON_PACKAGER = "npm install --save-dev electron-packager"
-PACKAGE_ELECTRON_APPLICATION = 'npx electron-packager . --out=../server/widgetron_app --ignore=node_modules --icon="{}"'.format
 CONDA_BUILD = "conda-mambabuild {} -c conda-forge"
 
 if WIN:
@@ -92,35 +105,38 @@ def parse_arguments():
 
 def copy_source_code(kwargs):
     # Copy python source into template package
+    dest = kwargs["temp_files"] / "server/widgetron_app/data"
+    dest.mkdir()
+
     if kwargs["python_source_dir"]:
         if Path(kwargs["python_source_dir"]).is_dir():
             shutil.copytree(
                 kwargs["python_source_dir"],
-                kwargs["temp_files"]
-                / "server/widgetron_app"
-                / Path(kwargs["python_source_dir"]).stem,
+                dest / Path(kwargs["python_source_dir"]).stem,
             )
         elif Path(kwargs["python_source_dir"]).is_file():
             shutil.copy(
                 kwargs["python_source_dir"],
-                kwargs["temp_files"] / "server" / "widgetron_app",
+                dest,
             )
 
 
 def copy_notebook(kwargs):
     # Copy notebook into template
     # Check filetype
+    dest = kwargs["temp_files"] / "server/widgetron_app/data"
     nb = Path(kwargs["file"])
+
     if nb.is_file():
         assert nb.suffix.lower() == ".ipynb", f"{nb} is not a notebook"
-        shutil.copy(nb, kwargs["temp_files"] / "server/widgetron_app")
+        shutil.copy(nb, dest)
     else:
         assert list(nb.glob("*.ipynb")), f"No notebooks found in {nb}"
         assert not kwargs[
             "python_source_dir"
         ], "-src may only be provided if -f is a single .ipynb file"
         shutil.copytree(
-            nb, kwargs["temp_files"] / f"server/widgetron_app/{nb.stem}"
+            nb, dest/nb.stem
         )
 
 
@@ -129,12 +145,21 @@ def package_electron_app(kwargs):
     cwd = Path().absolute()
 
     os.chdir(str(kwargs["temp_files"] / "electron"))
+    os.mkdir("build")
+    # assert icon.suffix.lower() == ".png", "WIP: only png currently supported"
+    shutil.copy(str(icon), f"build/icon{icon.suffix}")
 
     call("npm install .", shell=True)
-    call(
-        PACKAGE_ELECTRON_APPLICATION(icon),
-        shell=True,
-    )
+    call("npm run build", shell=True,)
+
+    dist = list(Path("dist").glob("*-unpacked"))[0]
+    os.chdir(dist)
+
+
+    with zipfile.ZipFile('../../../server/widgetron_app/ui.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zipdir('.', zipf)
+
+
     os.chdir(str(cwd))
 
 
